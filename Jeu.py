72494dddf032f49a758725da6797bf7975971ff0
Rosalie19 <rosalie.biedermann@gmail.com>
2022-01-05 10:46:07 +0100

    Update Jeu.py

--- Jeu.py
+++ Jeu.py
@@ -9,6 +9,96 @@ import os
 cloche = Lock()
 semaphore = Semaphore(0)
 
+""" etats player"""
+
+class Stateplayer:
+    wait = 1
+    echange = 2
+    faire_offre = 3
+    Sonner_cloche = 4
+
+def wait() :
+
+def echangercartes(i):
+    showcartes(i)
+    """proposer les cartes à échanger"""
+
+
+def Player(i, ):
+    while True :
+
+        if state[i] == 1:
+            """
+            showcartes(i)
+            showechanges()
+            echan = input("Voulez-vous accepter un des échanges proposés? (oui/non)")
+            if echan == oui:
+                state[i] = 2
+            else :
+                off = input ("Voulez-vouz faire une offre? (oui/non)")
+                if off == oui:
+                    state[i] = 3
+            """
+            rep = input("proposer offre = 1")
+            if rep == 1:
+                state[i] = 3
+
+        #proposer un echange
+        if state[i] == 2:
+            showechanges()
+            num = input("Quelle offre voulez-vous?")
+            cartesech = listeechanges[num[1]]
+
+
+        #proposer une offre
+        if state[i] == 3:
+
+            showcartes(i)
+            moy = input ("quel moyen de transport voulez vous echanger?")
+
+            cartes[]  # toutes les cartes de ce moyen de transport du joueurs
+
+            if cartes.lenght() > 3:
+                cartesech = cartes[0:2]
+            else:
+                cartesech = cartes
+
+
+            occupé = True
+            while occuppe == True:
+                if not sem.acquire():
+                    sem.release()
+                    print("Un échange est déjà en cours, veuillez patienter")
+                else:
+                    listechanges.append([i, cartesech[]])
+                    sem.release()
+
+
+
+
+
+        if state[i] == 4:
+
+
+"""pour montrer les cartes du joueur"""
+def showcartes(i):
+    print("Voulez-vous voir vos cartes? (oui/non)")
+    rep = input()
+    if rep == oui:
+        mq.send("ok", type = 1)
+
+""" afficher tous les échanges proposés"""
+def showechanges():
+    print("Voulez-vous voir les offres? (oui/non)")
+    rep = input()
+    if rep == oui:
+        mq.send("ok", type = 2)
+
+
+
+
+
+
 def creation():
     enseignes = ["avion","voiture","train","velo","pieton"]
     valeurs = [1,2,3,4,5]
