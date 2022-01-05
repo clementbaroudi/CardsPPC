54ec1445ff3918c95a486b8440c434402108d436
clementbaroudi <clement.baroudi@insa-lyon.fr>
2022-01-05 09:41:45 +0100

    Modification de l'indentation de jeu et ajout de la fonction de distribution des cartes

--- Jeu.py
+++ Jeu.py
@@ -3,26 +3,60 @@ import multiprocessing
 import threading
 from threading import Thread,Timer, Lock, Semaphore
 import sysv_ipc
+import random
 
+key = 128
 cloche = Lock()
 semaphore = Semaphore(0)
-Queue = sysv_ipc.MessageQueue(key)
 
 
+Queue = sysv_ipc.MessageQueue(key, sysv_ipc.IPC_CREAT)
+
+value = 1
+while value:
+    try:
+        value = int(input())
+    except:
+        print("Input error, try again!")
+    message = str(value).encode()
+    mq.send(message)
+
+mq.remove()
+    try:
+        value = int(input())
+    except:
+        print("Input error, try again!")
+    message = str(value).encode()
+    mq.send(message)
+
+mq.remove()
 
 
+def creation():
+    enseignes = ["avion","voiture","train","velo","pieton"]
+    valeurs = [1,2,3,4,5]
+    return [(x,y) for x in enseignes for y in valeurs]
 
+def get_carte(cartes):
+    # Fonction génératrice
+    NewCartes=cartes[0:5*nbJoueurs]
+    sabot = random.sample(NewCartes, 5*nbJoueurs)
+    while sabot:
+        # tant qu'il reste des cartes dans le sabot
+        yield sabot.pop()
 
-
-
-class Carte:
-
-	def __init__(self, MoyenDeTransport, Valeur):
-		self.MoyenDeTransport=MoyenDeTransport
-		self.Valeur=Valeur
 
 def distribuerCartes(nbJoueurs):
-
+	joueurs = [[] for i in range(nbJoueurs)]
+	# création du générateur de cartes
+	gen = get_carte(creation())
+	for i in range(5):
+	    for j in joueurs:
+	        try:
+	            j.append(next(gen))
+	        except StopIteration:
+	            pass
+	return joueurs
 
 def entreeClavier() :
     pass
@@ -35,10 +69,16 @@ def Game():
 
 if __name__ == "__main__":
 
-    nbJoueurs = int(input("Combien de joueurs y aura-t-il ?\n"))
-    echanges = Semaphore(0)
-    state = [State.wait for i in range(nbJoueurs)]
-    threads = [threading.Thread(target = Player, args = (i, )) for i in range(nbJoueurs)]
+	nbJoueurs = int(input("Combien de joueurs y aura-t-il ?\n"))
+	Deck=distribuerCartes(nbJoueurs)
+	for j in Deck:
+		print(j)
+	echanges = Semaphore(0)
+	state = [State.wait for i in range(nbJoueurs)]
+	threads = [threading.Thread(target = Player, args = (i, )) for i in range(nbJoueurs)]
+
+
+
 
 
 for thread in threads:
