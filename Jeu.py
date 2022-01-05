bbc50cf1441480a97774775621d07be02715f8a6
clementbaroudi <clement.baroudi@insa-lyon.fr>
2022-01-05 10:43:02 +0100

    Modif dans jeu

--- Jeu.py
+++ Jeu.py
@@ -4,33 +4,10 @@ import threading
 from threading import Thread,Timer, Lock, Semaphore
 import sysv_ipc
 import random
+import os
 
-key = 128
 cloche = Lock()
 semaphore = Semaphore(0)
-
-
-Queue = sysv_ipc.MessageQueue(key, sysv_ipc.IPC_CREAT)
-
-value = 1
-while value:
-    try:
-        value = int(input())
-    except:
-        print("Input error, try again!")
-    message = str(value).encode()
-    mq.send(message)
-
-mq.remove()
-    try:
-        value = int(input())
-    except:
-        print("Input error, try again!")
-    message = str(value).encode()
-    mq.send(message)
-
-mq.remove()
-
 
 def creation():
     enseignes = ["avion","voiture","train","velo","pieton"]
@@ -44,7 +21,6 @@ def get_carte(cartes):
     while sabot:
         # tant qu'il reste des cartes dans le sabot
         yield sabot.pop()
-
 
 def distribuerCartes(nbJoueurs):
 	joueurs = [[] for i in range(nbJoueurs)]
@@ -68,14 +44,40 @@ def Game():
     pass
 
 if __name__ == "__main__":
-
+    #Mise en place du jeu
+    key=128
+    queue=sysv_ipc.MessageQueue(key, sysv_ipc.IPC_CREAT)
+    print("Serveur en service")
 	nbJoueurs = int(input("Combien de joueurs y aura-t-il ?\n"))
 	Deck=distribuerCartes(nbJoueurs)
-	for j in Deck:
-		print(j)
+    with multiprocessing.Pool(processes = 4) as pool :
+        listen=True
+        while listen:
+            requete,t = queue.receive()
+            demande = str(requete.decode())
+            if demande == "Type 1":
+
+            elif demande == "Type 2":
+
+            elif demande == "Type 3":
+
+            elif demande == "Type 4":
+
+            elif demande == "Type 5":
+
+            elif demande == "Type 6":
+
+            else:
+                print("Requête non reconnue")
+                reponse = "Requête non reconnue"
+                reponse.encode()
+                queue.send(reponse)
+        print("Closing connexion")
+
+    '''
 	echanges = Semaphore(0)
 	state = [State.wait for i in range(nbJoueurs)]
-	threads = [threading.Thread(target = Player, args = (i, )) for i in range(nbJoueurs)]
+	threads = [threading.Thread(target = Player, args = (i, )) for i in range(nbJoueurs)]'''
 
 
 
